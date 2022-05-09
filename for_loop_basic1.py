8a92e08ad9222e1c161e416773b89a5db6ad5981
MakaelaRdz <100048409+MakaelaRdz@users.noreply.github.com>
2022-05-08 20:33:05 -0700

    intial commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,44 @@
+
+# Rules
+# 1 Print all integers from 0 to 150.
+# 2 Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+# 3 Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+# 4 Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+# 5 Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+# # # Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+
+# 1
+# for x in range(0, 151,):
+    # print(x)
+
+# 2
+# for x in range(5, 1000, 5):
+    # print(x)
+
+# 3
+# for x in range(0, 100,):
+#   print(x)
+#   if val =/ "5"
+#   print (Coding)
+#   elif val =/ 10 
+#   print (Coding Dojo)
+# 
+
+# 4 Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum
+# for x in range(0, 500000):
+    # print (sum)
+# 
+
+# 5 Count back by 4 starting at 2018
+# for x in range(2018, 0, -4):
+    # print(x)
+# 
+
+# 6 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+# lowNum = "6"
+# highNum = "30"
+# mult = "2"
+# for x in range (5 , 31 , *2):
+    # print (x)
